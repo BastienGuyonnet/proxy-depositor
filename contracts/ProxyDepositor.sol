63ceccab2958147087ec71f5efa0130bc10b870d
Bastien Guyonnet <bastien.guyonnet@tutanota.com>
2022-02-23 17:38:04 +0100

    staking now goes from token, to cToken, to gToken, initiated test file

--- contracts/ProxyDepositor.sol
+++ contracts/ProxyDepositor.sol
@@ -4,7 +4,10 @@ pragma solidity 0.8.11;
 
 import "./interfaces/IVoteEscrow.sol";
 import "./interfaces/IProxyToken.sol";
-import "./interaces/CErc20I.sol";
+import "./interfaces/ITokenMinter.sol";
+import "./interfaces/ILiquidityGauge.sol";
+import "./interfaces/CErc20I.sol";
+import "./interfaces/IComptroller.sol";
 import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
@@ -12,10 +15,11 @@ contract ProxyDepositor is AccessControlEnumerable {
     using SafeERC20 for IERC20;
 
     address public protocolToken; //protocol token that can be locked in exchange for voting power
-    address public veProtocolToken //voteEscrow version of the protocol token
+    address public veProtocolToken; //voteEscrow version of the protocol token
     address public proxyToken; //interest bearing wrapper token
     address[] public stakedTokens; //tokens that can be staked / supplied
     address public locker; //contract used to lock token
+    address public tokenMinter;
 
     //todo Though the voting part of the protocol should be the same accross the board,
     // the staking, lending of assets may differ. Hundred uses a compound style system,
@@ -24,6 +28,10 @@ contract ProxyDepositor is AccessControlEnumerable {
     address public masterChef; //contract handling pools and distributing rewards
     address public comptroller; //comptroller contract
 
+    /// @notice Error messages
+    string constant ACCESS_DENIED = "ACCESS DENIED";
+    string constant WRONG_TOKEN = "WRONG TOKEN";
+    string constant AMOUNT_EQUAL_ZERO = "AMOUNT = 0";
 
     /// @notice Helpful constants
     uint256 MAX_LOCK_TIME = 4 * 365 * 24 * 3600; // 4 years
@@ -32,15 +40,27 @@ contract ProxyDepositor is AccessControlEnumerable {
     bytes32 public constant STRATEGY = keccak256("STRATEGY");
     bytes32 public constant STRATEGIST = keccak256("STRATEGIST");
 
-    mapping (address => registeredStrategy) registeredStrategies;
-    mapping (address => Rewards[]) gaugeToRewards; 
+    ///@notice informational mappings
+    /// {registeredStrategies} - strategies that use the proxy depositor
+    /// {strategyToReward} - returns the rewards associated to a strategy
+    /// {tokenToPool} - returns the contract where token can be supplied and boosted
+    /// {tokenToGauge} - returns the contract where cToken can be staked
+    mapping (address => strategyInfo) registeredStrategies;
+    mapping (address => rewards[]) strategyToReward;
     mapping (address => address) tokenToPool;
+    mapping (address => address) tokenToGauge;
 
     ///@notice Relevant information about the strategy
     struct strategyInfo {
-        address strategy;
+        address strategy;// yeah, that's weird
         address wantToken;
-        uint256 rewards;
+        address gauge;
+    }
+
+    ///@notice reward model for a strategy
+    struct rewards {
+        address rewardToken;
+        uint256 percentage;
     }
 
     /**
@@ -48,10 +68,11 @@ contract ProxyDepositor is AccessControlEnumerable {
      * @param _locker contract to interact with to lock the protocol token (ex: veHND => 0x376020c5b0ba3fd603d7722381faa06da8078d8a)
      * @param _masterChef contract to interact with to stake tokens in pools and get rewards ()
      */
-    constructor(address _proxyToken, address _locker, address _masterChef) {
+    constructor(address _proxyToken, address _locker, address _masterChef, address _tokenMinter) {
         proxyToken = _proxyToken;
         locker = _locker;
         masterChef = _masterChef;
+        tokenMinter = _tokenMinter;
         protocolToken = IProxyToken(proxyToken).underlying();
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
@@ -60,19 +81,30 @@ contract ProxyDepositor is AccessControlEnumerable {
     // assumes the tokens have been transferred to this contracts, otherwise fetch them
     function depositToStake(address _token) public returns(bool){
         _onlyRegisteredStrategies();
+
+        // Get pool and gauge for token
         address pool = tokenToPool[_token];
-        require(pool != address(0), "UNKNOWN TOKEN");
+        address gauge = tokenToGauge[_token];
+        require(pool != address(0) && gauge != address(0), WRONG_TOKEN);
+
+        // Enter market, mint cToken
         uint256 tokenBalance = IERC20(_token).balanceOf(address(this));
         IERC20(_token).safeIncreaseAllowance(pool, tokenBalance);
         CErc20I(pool).mint(tokenBalance);
+
+        // Stake cToken into gauge
+        uint256 cTokenBalance = IERC20(pool).balanceOf(address(this));
+        IERC20(pool).safeIncreaseAllowance(gauge, cTokenBalance);
+        ILiquidityGauge(gauge).deposit(cTokenBalance, address(this), false);
+
         return true;
     }
 
     // user wants to lock the tokens
     // assumes protocol tokens are transferred to this contract beforehand
     // deposit governance token (ex: HND) for proxy token and lock
-    function depositToLock(uint256 _amount) public returns(bool){
-        require(_amount != 0, "AMOUNT EQUAL 0");
+    function depositToLock(uint256 _amount) public {
+        require(_amount != 0, AMOUNT_EQUAL_ZERO);
         uint256 protocolTokenBal = IERC20(protocolToken).balanceOf(address(this));
         uint256 unlockTime = block.timestamp + MAX_LOCK_TIME;
         IERC20(locker).safeIncreaseAllowance(locker, protocolTokenBal);
@@ -82,16 +114,14 @@ contract ProxyDepositor is AccessControlEnumerable {
         } else {
             _increaseLock(protocolTokenBal, unlockTime);
         }
-        return true;
     }
 
-    function _createLock(uint256 _amount, uint256 _unlockTime) internal returns(bool) {
+    function _createLock(uint256 _amount, uint256 _unlockTime) internal {
         IERC20(protocolToken).safeIncreaseAllowance(locker, _amount);
         IVoteEscrow(locker).create_lock(_amount, _unlockTime);
-        return true;
     }
 
-    function _increaseLock(uint256 _amount, uint256 _unlockTime) internal returns(bool) {
+    function _increaseLock(uint256 _amount, uint256 _unlockTime) internal {
         IERC20(protocolToken).safeIncreaseAllowance(locker, _amount);
         IVoteEscrow(locker).increase_amount(_amount);
         IVoteEscrow(locker).increase_unlock_time(_unlockTime);
@@ -103,19 +133,40 @@ contract ProxyDepositor is AccessControlEnumerable {
         return;
     }
 
-    //strategy comes to get the rewards -> out of the entered market for this strategy
-    function retrieveRewards() public returns (bool){
+    // strategy comes to get the rewards
+    // on HND, this means getting the HND rewards, sending only a strategies'
+    // share back to it, and up to aa certain bonus
+    function retrieveRewards() public {
         _onlyRegisteredStrategies();
         CTokenI[] memory tokens = new CTokenI[](1);
-        tokens[0] = registeredStrategies[msg.sender].wantToken;
-        comptroller.claimComp(address(this), tokens);
+        tokens[0] = CErc20I(registeredStrategies[msg.sender].wantToken);
+        ITokenMinter(tokenMinter).mint(registeredStrategies[msg.sender].gauge);
+        // Split and send to strat and voters
+    }
+
+    function voteForGaugeWeight(address _gauge_addr, uint256 _user_weight) external {
+        //todo decide how to implement this
+        //  users may choose
+        //  or, we do
     }
 
     function _onlyRegisteredStrategies() internal {
-        require(hasRole(STRATEGY, msg.sender), "ACCESS DENIED");
+        require(hasRole(STRATEGY, msg.sender), ACCESS_DENIED);
     }
 
-    function addStrategy(address _strategy, address _want) external onlyRole(DEFAULT_ADMIN_ROLE) returns(bool) {
+    function addStrategy(address _strategy, address _wantToken, address _gauge) external onlyRole(DEFAULT_ADMIN_ROLE){
+        require(hasRole(STRATEGY, msg.sender), ACCESS_DENIED);
+        strategyInfo memory strategy = strategyInfo(_strategy, _wantToken, _gauge);
+        registeredStrategies[_strategy] = strategy;
+    }
 
+    function setTokenToGauge(address _token, address _gauge) external {
+        require(hasRole(STRATEGIST, msg.sender), ACCESS_DENIED);
+        tokenToGauge[_token] = _gauge;
+    }
+
+    function setTokenToPool(address _token, address _pool) external {
+        require(hasRole(STRATEGIST, msg.sender), ACCESS_DENIED);
+        tokenToPool[_token] = _pool;
     }
 }
--- test/proxy-depositor.js
+++ test/proxy-depositor.js
@@ -4,7 +4,96 @@ const { solidity } = require("ethereum-waffle");
 chai.use(solidity);
 const { expect } = chai;
 
+// Custom utility function
+
 const moveTimeForward = async (seconds) => {
     await network.provider.send("evm_increaseTime", [seconds]);
     await network.provider.send("evm_mine");
 }
+
+// Registry
+/// Tokens
+const protocolTokenAddress = "0x10010078a54396F62c96dF8532dc2B4847d47ED3"; // HND
+const veProtocolTokenAddress= "0x376020c5B0ba3Fd603d7722381fAA06DA8078d8a" // veHND
+const WantTokenAddress = "0x04068DA6C83AFCFA0e13ba15A6696662335D5B75"; // usdc
+const CoumpoundWantTokenAddress = "0x243e33aa7f6787154a8e59d3c27a66db3f8818ee";// hUSDC
+const GaugeWantTokenAddress = "0x110614276F7b9Ae8586a1C1D9Bc079771e2CE8cF";// hUSDC gauge
+
+/// Contracts
+const comptroller = "0x0F390559F258eB8591C8e31Cf0905E97cf36ACE2";
+const tokenMinter = "0x42b458056f887fd665ed6f160a59afe932e1f559";
+const gaugeController = "0xb1c4426C86082D91a6c097fC588E5D5d8dD1f5a8";
+
+/// EOA
+const strategistAddress = "0x1E71AEE6081f62053123140aacC7a06021D77348";
+const wantHolderAddress = "0xA9497FD9D1dD0d00DE1Bf988E0e36794848900F9";
+
+describe('Proxy-Depositor', () => {
+    // ContractFactories
+    /// Tested
+    let ProxyDepositor;
+    let ProxyToken;
+
+    /// Already exists
+    let ProtocolToken;
+    let WantToken;
+    let CompoundWantToken;
+    let GaugeWantToken;
+
+    // Contracts
+    /// Tested
+    let proxyDepositor;
+    let proxyToken;
+
+    /// Already exists
+    let protocolToken;
+    let wantToken;
+    let compoundWantToken;
+    let gaugeWantToken;
+
+    // Accounts and more
+    let self;
+    let selfAddress;
+    let owner;
+    
+    beforeEach( async () => {
+        //reset network
+        await network.provider.request({
+            method: "hardhat_reset",
+            params: [
+                {
+                    forking: {
+                        jsonRpcUrl: "https://late-wild-fire.fantom.quiknode.pro/",
+                        // jsonRpcUrl: "https://rpc.ftm.tools/",
+                    },
+                },
+            ],
+        });
+
+        // get signers
+        [owner] = await ethers.getSigners();
+        await hre.network.provider.request({
+            method: "hardhat_impersonateAccount",
+            params: [wantHolderAddress],
+        });
+        self = await ethers.provider.getSigner(wantHolderAddress);
+        selfAddress = await self.getAddress();
+
+        // get artifacts
+        ProxyDepositor = await ethers.getContractFactory("ProxyDepositor");
+        WantToken = await ethers.getContractFatctory("@openzeppelin/contracts/token/ERC20/ERC20.sol:ERC20");
+        ProtocolToken = await ethers.getContractFactory("Hundred");
+        ProxyToken = await ethers.getContractFactory("ProxyToken");
+
+        //deploy
+        proxyToken = await ProxyToken.deploy(protocolTokenAddress);
+        console.log("proxyToken address: ", proxyToken.address);
+    });
+
+    describe("ProxyToken and ProxyDepositor tests", () => {
+        it("should allow deposits of usdc", async () => {
+            const userBalance = await wantToken.balanceOf(selfAddress);
+            
+        });
+    });
+});
