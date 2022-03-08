abbdc0f1c3699ee1e657d74a7d2e6544c26e0b15
Bastien Guyonnet <bastien.guyonnet@tutanota.com>
2022-03-08 19:17:01 +0100

    updated proxydepositor according to recent proxytoken changes, now handles loose protocol token balance

--- contracts/ProxyDepositor.sol
+++ contracts/ProxyDepositor.sol
@@ -32,6 +32,7 @@ contract ProxyDepositor is AccessControlEnumerable {
     address public comptroller; //comptroller contract
 
     /// @notice Error messages
+    string constant WRONG_INPUT = "WRONG INPUT";
     string constant ACCESS_DENIED = "ACCESS DENIED";
     string constant WRONG_TOKEN = "WRONG TOKEN";
     string constant AMOUNT_EQUAL_ZERO = "AMOUNT = 0";
@@ -39,25 +40,32 @@ contract ProxyDepositor is AccessControlEnumerable {
 
     /// @notice Helpful constants
     uint256 constant MAX_LOCK_TIME = 4 * 365 * 24 * 3600; // 4 years
+    uint256 constant WEEK = 7 * 24 * 3600;
+
+    /// Time until tokens can be unlocked
+    uint256 unlockTimeWeek;
+    uint256 balanceToLock;
 
     /// @notice Roles
-    bytes32 public constant STRATEGY = keccak256("STRATEGY");
     bytes32 public constant STRATEGIST = keccak256("STRATEGIST");
+
+    /// @notice Variables used to split the rewards
+    uint256 public constant PERCENT_DIVISOR = 10_000;
+    uint256 public veProviderSplit = 1000;
+    uint256 public permanentLockSplit = 500;
+    uint256 public strategySplit = 8500;
 
     ///@notice informational mappings
     /// {registeredStrategies} - strategies that use the proxy depositor
     /// {strategyToReward} - returns the rewards associated to a strategy
-    /// {tokenToPool} - returns the contract where token can be supplied and boosted
-    /// {tokenToGauge} - returns the contract where cToken can be staked
-    mapping (address => strategyInfo) registeredStrategies;
-    mapping (address => rewards[]) strategyToReward;
-    mapping (address => address) tokenToPool;
-    mapping (address => address) tokenToGauge;
+    mapping (address => strategyInfo) public registeredStrategies;
+    address[] public strategies;
 
     ///@notice Relevant information about the strategy
     struct strategyInfo {
-        address strategy;// yeah, that's weird
+        uint256 id;
         address wantToken;
+        address pool;
         address gauge;
     }
 
@@ -83,15 +91,17 @@ contract ProxyDepositor is AccessControlEnumerable {
         console.log(msg.sender);
     }
 
-    // strategy wants to deposit tokens in rewarding pools, boosted
-    // assumes the tokens have been transferred to this contracts, otherwise fetch them
-    function depositToStake(address _token) public {
+    /// @notice Strategies will use this to deposit the tokens and get yield
+    /// 
+    function depositToStake(address _token) public returns (uint256) {
         _onlyRegisteredStrategies();
 
+        // Get strategy info
+        strategyInfo memory strategy = registeredStrategies[msg.sender]; 
+
         // Get pool and gauge for token
-        address pool = tokenToPool[_token];
-        address gauge = tokenToGauge[_token];
-        require(pool != address(0) && gauge != address(0), WRONG_TOKEN);
+        address pool = strategy.pool;
+        address gauge = strategy.gauge;
 
         // Enter market, mint cToken
         uint256 tokenBalance = IERC20(_token).balanceOf(address(this));
@@ -102,44 +112,65 @@ contract ProxyDepositor is AccessControlEnumerable {
         uint256 cTokenBalance = IERC20(pool).balanceOf(address(this));
         IERC20(pool).safeIncreaseAllowance(gauge, cTokenBalance);
         ILiquidityGauge(gauge).deposit(cTokenBalance, address(this), false);
+
+        return ILiquidityGauge(gauge).balanceOf(address(this));
     }
 
     // user wants to lock the tokens
-    // assumes protocol tokens are transferred to this contract beforehand
     // deposit governance token (ex: HND) for proxy token and lock
-    function depositToLock(uint256 _amount) public {
-        require(_amount != 0, AMOUNT_EQUAL_ZERO);
+    function depositToLock(uint256 amount, address user) public {
+        IERC20(protocolToken).safeTransferFrom(proxyToken, address(this), amount);
+
+        uint256 newUnlockTime = block.timestamp + MAX_LOCK_TIME;
+        uint256 newUnlockTimeWeek = (newUnlockTime / WEEK) * WEEK;
+
         uint256 protocolTokenBal = IERC20(protocolToken).balanceOf(address(this));
-        uint256 unlockTime = block.timestamp + MAX_LOCK_TIME;
         IERC20(locker).safeIncreaseAllowance(locker, protocolTokenBal);
+
         // If no protocol token is locked yet, a new lock needs to be created
         if(IVoteEscrow(locker).locked(address(this)).amount == 0) {
-            _createLock(protocolTokenBal, unlockTime);
+            _createLock(protocolTokenBal, newUnlockTimeWeek);
         } else {
-            _increaseLock(protocolTokenBal, unlockTime);
+            _increaseLock(protocolTokenBal, newUnlockTimeWeek);
+        }
+
+        // mint the proxy token, send to user
+        // proxyToken to protocolToken is 1:1
+        IProxyToken(proxyToken).mint(user, amount);
+    }
+
+    function _createLock(uint256 _amount, uint256 _unlockTimeWeek) internal {
+        IERC20(protocolToken).safeIncreaseAllowance(locker, _amount);
+        IVoteEscrow(locker).create_lock(_amount, _unlockTimeWeek);
+        unlockTimeWeek = _unlockTimeWeek;
+    }
+
+    function _increaseLock(uint256 _amount, uint256 _unlockTimeWeek) internal {
+        // Making sure the lock can be increased to this unlockTimeWeek
+        // because the voteEscrow will revert if the previous lock ends the same week
+        if(_unlockTimeWeek > unlockTimeWeek) {
+            IERC20(protocolToken).safeIncreaseAllowance(locker, _amount);
+            IVoteEscrow(locker).increase_amount(_amount);
+            IVoteEscrow(locker).increase_unlock_time(_unlockTimeWeek);
+            unlockTimeWeek = _unlockTimeWeek;
+            balanceToLock = 0;
+        } else {
+            // This balance needs to stay in the contract
+            // instead of being sent as reward
+            balanceToLock += _amount;
         }
     }
 
-    function _createLock(uint256 _amount, uint256 _unlockTime) internal {
-        IERC20(protocolToken).safeIncreaseAllowance(locker, _amount);
-        IVoteEscrow(locker).create_lock(_amount, _unlockTime);
-    }
-
-    function _increaseLock(uint256 _amount, uint256 _unlockTime) internal {
-        IERC20(protocolToken).safeIncreaseAllowance(locker, _amount);
-        IVoteEscrow(locker).increase_amount(_amount);
-        IVoteEscrow(locker).increase_unlock_time(_unlockTime);
-    }
-
-    //todo current amount passed is expressend in gaugeToken
-    //todo should I rework this so that a token amount is passed in 
+    //todo current amount passed is expressed in gaugeToken
     function withdrawFromStake(address _token, uint256 amount) external {
         _onlyRegisteredStrategies();
 
+        // Get strategy info
+        strategyInfo memory strategy = registeredStrategies[msg.sender]; 
+
         // Get pool and gauge for token
-        address pool = tokenToPool[_token];
-        address gauge = tokenToGauge[_token];
-        require(pool != address(0) && gauge != address(0), WRONG_TOKEN);
+        address pool = strategy.pool;
+        address gauge = strategy.gauge;
 
         // use gToken to get cToken
         uint256 amountToWithdraw = Math.min(ILiquidityGauge(gauge).balanceOf(address(this)),amount);
@@ -155,51 +186,75 @@ contract ProxyDepositor is AccessControlEnumerable {
 
         // send all token of the depositor to the strategy
         uint256 tokenBal = IERC20(_token).balanceOf(address(this));
-        console.log("tokenBal :", tokenBal);
         IERC20(_token).safeTransfer(msg.sender, tokenBal);
-    }
-
-    // user wants to retrieve his tokens
-    function withdrawFromLock() public {
-        //todo implement this or remove if we consider locked token to be eternally locked
-        return;
     }
 
     // strategy comes to get the rewards
     // on HND, this means getting the HND rewards, sending only a strategies'
-    // share back to it, and up to aa certain bonus
+    // share back to it, and up to a certain bonus
     function retrieveRewards() public {
         _onlyRegisteredStrategies();
-        CTokenI[] memory tokens = new CTokenI[](1);
-        tokens[0] = CErc20I(registeredStrategies[msg.sender].wantToken);
         ITokenMinter(tokenMinter).mint(registeredStrategies[msg.sender].gauge);
 
-        // Split and send to strat and voters
+        // Split and send to strat and voters, ignore balance that is meant to be locked
+        uint256 protocolTokenBalance = IERC20(protocolToken).balanceOf(address(this)) - balanceToLock;
+        uint256 providerRewards = protocolTokenBalance * veProviderSplit / PERCENT_DIVISOR;
+        uint256 permanentLockRewards = protocolTokenBalance * permanentLockSplit / PERCENT_DIVISOR;
+        uint256 strategyRewards = protocolTokenBalance * strategySplit / PERCENT_DIVISOR;
+
+        IERC20(protocolToken).safeTransfer(msg.sender, strategyRewards);
+        IERC20(protocolToken).safeTransfer(proxyToken, providerRewards); // assuming proxyToken manages handing out thos rewards to users
+        _increaseLock(permanentLockRewards, MAX_LOCK_TIME);
+
+        IProxyToken(proxyToken).updateRewards(providerRewards);
     }
 
-    function voteForGaugeWeight(address _gauge_addr, uint256 _user_weight) external {
-        //todo decide how to implement this
-        //  users may choose
-        //  or, we do
-    }
-
-    function _onlyRegisteredStrategies() internal {
-        require(registeredStrategies[msg.sender].strategy == msg.sender, ACCESS_DENIED);
-    }
-
-    function addStrategy(address _strategy, address _wantToken, address _gauge) external onlyRole(DEFAULT_ADMIN_ROLE){
+    /// @notice change the voting allocation
+    /// Both arrays need to be of same length
+    /// Total of weights should add up to 100
+    function setVoteForGaugeWeightsManually(address[] calldata _gauge_addrs, uint256[] calldata _user_weights) external {
         require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), ACCESS_DENIED);
-        strategyInfo memory strategy = strategyInfo(_strategy, _wantToken, _gauge);
+        require(_gauge_addrs.length == _user_weights.length, WRONG_INPUT);
+        require(_gauge_addrs.length == strategies.length, WRONG_INPUT);
+
+        uint256 total;
+        for (uint256 i; i < _gauge_addrs.length; i++) {
+            total = total + _user_weights[i];
+            //set the vote
+        }
+
+        require(total == 100, WRONG_INPUT);
+    }
+
+    function _onlyRegisteredStrategies() internal view {
+        require(registeredStrategies[msg.sender].wantToken != address(0), ACCESS_DENIED);
+    }
+
+    /// @notice register a new strategy if possible
+    function addStrategy(address _strategy, address _wantToken, address _gauge, address _pool) external {
+        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), ACCESS_DENIED);
+        require(registeredStrategies[_strategy].wantToken == address(0), WRONG_INPUT);
+        strategyInfo memory strategy = strategyInfo(strategies.length, _wantToken, _gauge, _pool);
         registeredStrategies[_strategy] = strategy;
+        strategies.push(_strategy);
     }
 
-    function setTokenToGauge(address _token, address _gauge) external {
-        require(hasRole(STRATEGIST, msg.sender), ACCESS_DENIED);
-        tokenToGauge[_token] = _gauge;
+    /// @notice Check that the address is a strategy, remove it from the strategies array
+    /// assumes the strategy will check it has retrieved its funds
+    function removeStrategy(address _strategy) external {
+        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), ACCESS_DENIED);
+        require(registeredStrategies[_strategy].wantToken != address(0), WRONG_INPUT);
+        strategies[registeredStrategies[_strategy].id] = strategies[strategies.length - 1];
+        strategies.pop();
+        delete registeredStrategies[_strategy];
     }
 
-    function setTokenToPool(address _token, address _pool) external {
+    /// @notice How to allocate rewards
+    function setRewardSplit(uint256 _veProviderSplit, uint256 _permanentLockSplit, uint256 _strategySplit) external {
         require(hasRole(STRATEGIST, msg.sender), ACCESS_DENIED);
-        tokenToPool[_token] = _pool;
+        require(_veProviderSplit + _permanentLockSplit + _strategySplit == PERCENT_DIVISOR, WRONG_INPUT);
+        veProviderSplit = _veProviderSplit;
+        permanentLockSplit = _permanentLockSplit;
+        strategySplit = _strategySplit;
     }
 }
